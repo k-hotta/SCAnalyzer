d9b7806e27353171d4920f98759c616fdac457bf
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-10-06 15:10:29 +0200

    initialize as a maven repository

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/App.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
