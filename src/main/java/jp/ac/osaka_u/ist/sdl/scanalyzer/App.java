d8d76c2d2eb8b2c0e79903f4b5051d163e24cbc3
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-10-07 18:34:36 +0200

    testec ORMLite

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/App.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/App.java
@@ -1,23 +1,38 @@
 package jp.ac.osaka_u.ist.sdl.scanalyzer;
 
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Revision;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
+
 /**
  * Hello world!
- *
+ * 
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        Logger logger = LogManager.getLogger(App.class);
-        Logger eLogger = LogManager.getLogger("error");
-        logger.trace("trace");
-        logger.debug("debug");
-        logger.info("info");
-        eLogger.warn("warn");
-        eLogger.error("error");
-        eLogger.fatal("fatal");
-    }
+public class App {
+	public static void main(String[] args) throws Exception {
+		Logger logger = LogManager.getLogger(App.class);
+		Logger eLogger = LogManager.getLogger("error");
+
+		String dbUrl = "jdbc:sqlite:C:/Users/k-hotta/git/SCAnalyzer/src/test/resources/test.db";
+		ConnectionSource connectionSource = new JdbcConnectionSource(dbUrl);
+
+		Dao<Revision, Long> revisionDao = DaoManager.createDao(
+				connectionSource, Revision.class);
+		TableUtils.createTable(connectionSource, Revision.class);
+
+		Revision newRevision = new Revision(1, "init");
+		revisionDao.create(newRevision);
+
+		Revision retrieved = revisionDao.queryForId((long) 1);
+		logger.info(retrieved.getId() + "," + retrieved.getIdentifier());
+
+		connectionSource.close();
+	}
 }
