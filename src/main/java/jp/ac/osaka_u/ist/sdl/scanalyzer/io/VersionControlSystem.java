1d44b1c932c11dbcfa4522c3f0057e146a1ba7f7
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-10-24 18:11:58 +0200

    add an enum to represent version control system

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/VersionControlSystem.java
@@ -0,0 +1,16 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.io;
+
+/**
+ * This enum describes version control systems under support.
+ * 
+ * @author k-hotta
+ * 
+ */
+public enum VersionControlSystem {
+
+	/**
+	 * Subversion
+	 */
+	SVN;
+
+}
