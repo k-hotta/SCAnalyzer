717431d578de83fb97648cbfc545fb7995082637
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-10-27 11:00:29 +0100

    add an interface to represent the atomic elements of source code

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/data/IAtomicElement.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.data;
+
+/**
+ * This interface describes the atomic element of source code which is of
+ * interest in the investigation. <br>
+ * The example of atomic elements would be, Tokens, Lines, and Statements.
+ * 
+ * @author k-hotta
+ * 
+ */
+public interface IAtomicElement {
+	
+}
