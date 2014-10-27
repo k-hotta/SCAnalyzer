7a916e7d9d7cc8190200fa97d5c27477e91f4152
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-10-27 18:20:19 +0100

    implement equals, hashCode, and toString of SegmentContent

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/data/SegmentContent.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/data/SegmentContent.java
@@ -101,4 +101,42 @@ public class SegmentContent<E extends IAtomicElement> {
 		return contents;
 	}
 
+	/**
+	 * Judge whether the given object equals to this object. <br>
+	 * 
+	 * @return <code>true</code> if the given object is an instance of
+	 *         {@link SegmentContent} and the owner segments of the two objects
+	 *         are the same to each other, <code>false</code> otherwise.
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (!(obj instanceof SegmentContent)) {
+			return false;
+		}
+
+		final SegmentContent<?> another = (SegmentContent<?>) obj;
+
+		return this.segment.equals(another.getSegment());
+	}
+
+	/**
+	 * Return a hash value, which equals to the hash code of {@link Segment}.
+	 */
+	@Override
+	public int hashCode() {
+		return this.segment.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		
+		builder.append(this.segment + "\n");
+		for (final E content : this.contents.values()) {
+			builder.append(content);
+		}
+		
+		return builder.toString();
+	}
+	
 }
