c3329ea9177ab5f88aad193b57629e5f0521914b
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-03 16:10:25 +0100

    implement a mapping algorithm of tokens with traditional unix diff

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/mapping/TraditionalDiffTokenMapper.java
@@ -0,0 +1,352 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.mapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.FileChange;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.SourceFile;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Token;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
+import difflib.Delta;
+import difflib.DiffUtils;
+import difflib.Patch;
+
+/**
+ * This class provides mapping of tokens between two consecutive versions with
+ * traditional UNIX-Diff algorithm. This mapper never maps two tokens that are
+ * in different source files. In other words, this mapper does not consider code
+ * move between files.
+ * 
+ * @author k-hotta
+ *
+ */
+public class TraditionalDiffTokenMapper implements IProgramElementMapper<Token> {
+
+	/**
+	 * This map contains the mapping of tokens
+	 */
+	private final ConcurrentMap<Token, Token> mapping;
+
+	/**
+	 * The constructor
+	 */
+	public TraditionalDiffTokenMapper() {
+		this.mapping = new ConcurrentHashMap<>();
+	}
+
+	@Override
+	public Token getNext(Token previous) {
+		if (previous == null) {
+			throw new IllegalArgumentException("the given token is null");
+		}
+
+		return mapping.get(previous);
+	}
+
+	@Override
+	public boolean prepare(Version<Token> previousVersion,
+			Version<Token> nextVersion) {
+		if (previousVersion == null) {
+			throw new IllegalArgumentException(
+					"the given previous version is null");
+		}
+
+		if (nextVersion == null) {
+			throw new IllegalArgumentException("the given next version is null");
+		}
+
+		// clear the existing mapping
+		this.mapping.clear();
+
+		final List<TraditionalDiffTokenMappingTask> tasks = prepareTasks(
+				previousVersion, nextVersion);
+
+		ExecutorService pool = Executors.newCachedThreadPool();
+
+		try {
+			final List<Future<Map<Token, Token>>> futures = new ArrayList<>();
+
+			for (final TraditionalDiffTokenMappingTask task : tasks) {
+				futures.add(pool.submit(task));
+			}
+
+			for (final Future<Map<Token, Token>> future : futures) {
+				try {
+					this.mapping.putAll(future.get());
+				} catch (Exception e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+
+		} finally {
+			pool.shutdown();
+		}
+
+		return true;
+	}
+
+	/**
+	 * Construct tasks to detect token mapping. Each task performs mapping
+	 * between a pair of files.
+	 * 
+	 * @param previousVersion
+	 *            the previous version
+	 * @param nextVersion
+	 *            the next version
+	 * @return a list of constructed tasks
+	 */
+	private List<TraditionalDiffTokenMappingTask> prepareTasks(
+			final Version<Token> previousVersion,
+			final Version<Token> nextVersion) {
+		final List<TraditionalDiffTokenMappingTask> tasks = new ArrayList<>();
+
+		final Map<Long, SourceFile<Token>> previousFiles = new TreeMap<Long, SourceFile<Token>>();
+		previousFiles.putAll(previousVersion.getSourceFiles());
+
+		final Map<Long, SourceFile<Token>> nextFiles = new TreeMap<Long, SourceFile<Token>>();
+		nextFiles.putAll(nextVersion.getSourceFiles());
+
+		for (final FileChange<Token> fileChange : nextVersion.getFileChanges()
+				.values()) {
+			switch (fileChange.getType()) {
+			case ADD: {
+				final SourceFile<Token> newSourceFile = nextFiles
+						.remove(fileChange.getNewSourceFile().getId());
+				if (newSourceFile == null) {
+					throw new IllegalStateException("cannot find "
+							+ fileChange.getNewSourceFile() + " in version "
+							+ nextVersion.getId());
+				}
+
+				tasks.add(constructTask(null, newSourceFile, Type.ADD));
+
+				break;
+			}
+			case DELETE: {
+				final SourceFile<Token> oldSourceFile = previousFiles
+						.remove(fileChange.getOldSourceFile().getId());
+				if (oldSourceFile == null) {
+					throw new IllegalStateException("cannot find "
+							+ fileChange.getOldSourceFile() + " in version "
+							+ previousVersion.getId());
+				}
+
+				tasks.add(constructTask(oldSourceFile, null, Type.DELETE));
+
+				break;
+			}
+			case MODIFY:
+			case RELOCATE: {
+				final SourceFile<Token> oldSourceFile = previousFiles
+						.remove(fileChange.getOldSourceFile().getId());
+				if (oldSourceFile == null) {
+					throw new IllegalStateException("cannot find "
+							+ fileChange.getOldSourceFile() + " in version "
+							+ previousVersion.getId());
+				}
+
+				final SourceFile<Token> newSourceFile = nextFiles
+						.remove(fileChange.getNewSourceFile().getId());
+				if (newSourceFile == null) {
+					throw new IllegalStateException("cannot find "
+							+ fileChange.getNewSourceFile() + " in version "
+							+ nextVersion.getId());
+				}
+
+				tasks.add(constructTask(oldSourceFile, newSourceFile,
+						Type.MODIFY));
+
+				break;
+			} // this is the end of case MODIFY-RELOCATE
+			} // this is the end of switch-statement
+		}
+
+		if (previousFiles.size() != nextFiles.size()) {
+			// the remaining files are stable ones
+			// the numbers of unchanged files must be same between two versions
+			throw new IllegalStateException(
+					"the number of remaining files in the previous version does not match to that in the next version");
+		}
+
+		// construct tasks for unchanged files
+		final List<Long> stableFileIds = new ArrayList<Long>();
+		stableFileIds.addAll(previousFiles.keySet());
+		for (final long fileId : stableFileIds) {
+			final SourceFile<Token> oldSourceFile = previousFiles
+					.remove(fileId);
+			final SourceFile<Token> newSourceFile = nextFiles.remove(fileId);
+
+			if (oldSourceFile == null || newSourceFile == null) {
+				throw new IllegalStateException("file " + fileId
+						+ " does not seem to unstable");
+			}
+
+			tasks.add(constructTask(oldSourceFile, newSourceFile, Type.STABLE));
+		}
+
+		if (!nextFiles.isEmpty()) {
+			throw new IllegalStateException("some files remain unmapped");
+		}
+
+		return tasks;
+	}
+
+	/**
+	 * Construct a task with the specified values.
+	 * 
+	 * @param oldSourceFile
+	 *            the old source file
+	 * @param newSourceFile
+	 *            the new source file
+	 * @param type
+	 *            the type
+	 * @return a constructed task
+	 */
+	private TraditionalDiffTokenMappingTask constructTask(
+			final SourceFile<Token> oldSourceFile,
+			final SourceFile<Token> newSourceFile, final Type type) {
+		return new TraditionalDiffTokenMappingTask(oldSourceFile,
+				newSourceFile, type);
+	}
+
+	/**
+	 * The internal representation of change type. Note that there does not
+	 * exist RELOCATE, which will be treated in the same way of MODIFY. In
+	 * addition, this enum has STABLE, which means that the file was not changed
+	 * between the two versions.
+	 * 
+	 * @author k-hotta
+	 *
+	 */
+	private enum Type {
+		ADD, MODIFY, DELETE, STABLE;
+	}
+
+	/**
+	 * This class represents a task of mapping. Each task performs mapping a
+	 * source file.
+	 * 
+	 * @author k-hotta
+	 *
+	 */
+	private class TraditionalDiffTokenMappingTask implements
+			Callable<Map<Token, Token>> {
+
+		private final SourceFile<Token> oldSourceFile;
+
+		private final SourceFile<Token> newSourceFile;
+
+		private final Type type;
+
+		public TraditionalDiffTokenMappingTask(
+				final SourceFile<Token> oldSourceFile,
+				final SourceFile<Token> newSourceFile, final Type type) {
+			this.oldSourceFile = oldSourceFile;
+			this.newSourceFile = newSourceFile;
+			this.type = type;
+		}
+
+		@Override
+		public Map<Token, Token> call() throws Exception {
+			final Map<Token, Token> mapping = new HashMap<Token, Token>();
+
+			switch (type) {
+			case ADD:
+				break; // do nothing for file addition
+			case DELETE:
+				mappingDeletedFiles(mapping);
+				break;
+			case MODIFY:
+				mappingModifiedFiles(mapping);
+				break;
+			case STABLE:
+				mappingStableFiles(mapping);
+				break;
+			}
+
+			return mapping;
+		}
+
+		/**
+		 * Mapping for deleted files. All the tokens in old source file will be
+		 * mapped to <code>null</code> through this operation.
+		 * 
+		 * @param mapping
+		 *            the mapping where the results stored
+		 */
+		private void mappingDeletedFiles(final Map<Token, Token> mapping) {
+			for (final Token token : this.oldSourceFile.getContents().values()) {
+				mapping.put(token, null);
+			}
+		}
+
+		/**
+		 * Mapping for modified files. All the tokens in old source file will be
+		 * mapped to tokens in new source file based on diff information between
+		 * two files.
+		 * 
+		 * @param mapping
+		 *            the mapping where the results stored
+		 */
+		private void mappingModifiedFiles(final Map<Token, Token> mapping) {
+			final List<Token> oldTokens = new ArrayList<Token>();
+			oldTokens.addAll(oldSourceFile.getContents().values());
+
+			final List<Token> newTokens = new ArrayList<Token>();
+			newTokens.addAll(newSourceFile.getContents().values());
+
+			final Patch<Token> patch = DiffUtils.diff(oldTokens, newTokens);
+			for (final Delta<Token> delta : patch.getDeltas()) {
+				final List<Token> deletedTokens = delta.getOriginal()
+						.getLines();
+
+				for (final Token deletedToken : deletedTokens) {
+					mapping.put(deletedToken, null);
+				}
+				oldTokens.removeAll(deletedTokens);
+
+				final List<Token> addedTokens = delta.getRevised().getLines();
+				newTokens.removeAll(addedTokens);
+			}
+
+			if (oldTokens.size() != newTokens.size()) {
+				// the remaining tokens are unchanged tokens
+				// hence, the number of tokens must be the same
+				throw new IllegalStateException(
+						"the number of remaining tokens are not the same. Old: "
+								+ oldTokens.size() + " New: "
+								+ newTokens.size());
+			}
+
+			for (int index = 0; index < oldTokens.size(); index++) {
+				assert oldTokens.get(index).equals(newTokens.get(index));
+				mapping.put(oldTokens.get(index), newTokens.get(index));
+			}
+		}
+
+		/**
+		 * Mapping for stable files. Every token in old source file will be
+		 * mapped to the token itself through this operation.
+		 * 
+		 * @param mapping
+		 *            the mapping where the results stored
+		 */
+		private void mappingStableFiles(final Map<Token, Token> mapping) {
+			for (final Token token : this.oldSourceFile.getContents().values()) {
+				mapping.put(token, token);
+			}
+		}
+
+	}
+
+}
