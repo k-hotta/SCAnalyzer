ca5c13ad534169b7f91afa65499200b734c6eced
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-05 12:10:36 +0100

    add code fragment mapping with volatile data

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/data/CodeFragmentMapping.java
@@ -0,0 +1,174 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.data;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCodeFragmentMapping;
+
+/**
+ * This class represents mapping between code fragments.
+ * 
+ * @author k-hotta
+ *
+ * @param <E>
+ *            the type of program element
+ */
+public class CodeFragmentMapping<E extends IProgramElement> implements
+		IDataElement<DBCodeFragmentMapping> {
+
+	/**
+	 * The id of this mapping
+	 */
+	private final long id;
+
+	/**
+	 * The core of this mapping
+	 */
+	private final DBCodeFragmentMapping core;
+
+	/**
+	 * The old code fragment of this mapping
+	 */
+	private CodeFragment<E> oldCodeFragment;
+
+	/**
+	 * The new code fragment of this mapping
+	 */
+	private CodeFragment<E> newCodeFragment;
+
+	public CodeFragmentMapping(final DBCodeFragmentMapping core) {
+		this.id = core.getId();
+		this.core = core;
+		this.oldCodeFragment = null;
+		this.newCodeFragment = null;
+	}
+
+	@Override
+	public long getId() {
+		return id;
+	}
+
+	@Override
+	public DBCodeFragmentMapping getCore() {
+		return core;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (!(obj instanceof CodeFragmentMapping)) {
+			return false;
+		}
+
+		final CodeFragmentMapping<?> another = (CodeFragmentMapping<?>) obj;
+
+		return this.core.equals(another.getCore());
+	}
+
+	@Override
+	public int hashCode() {
+		return this.core.hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return this.core.toString();
+	}
+
+	/**
+	 * Get the old code fragment of this mapping.
+	 * 
+	 * @return the old code fragment of this mapping
+	 * 
+	 * @throws IllegalStateException
+	 *             if the old code fragment and/or the new code fragment have
+	 *             not been set
+	 */
+	public CodeFragment<E> getOldCodeFragment() {
+		if (oldCodeFragment == null && newCodeFragment == null) {
+			throw new IllegalStateException(
+					"the old code fragment and/or the new code fragment have not been set");
+		}
+
+		return this.oldCodeFragment;
+	}
+
+	/**
+	 * Set the old code fragment of this mapping with the specified one.
+	 * 
+	 * @param oldCodeFragment
+	 *            the old code fragment to be set
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if any of the following statements hold: (1) the given old
+	 *             code fragment is <code>null</code> but that in the core is
+	 *             not <code>null</code>, (2) the given old code fragment is not
+	 *             <code>null</code> but that in the core is <code>null</code>,
+	 *             (3) the given old code fragment does not match to that in the
+	 *             core
+	 */
+	public void setOldCodeFragment(final CodeFragment<E> oldCodeFragment) {
+		if (oldCodeFragment != null) {
+			if (this.core.getOldCodeFragment() == null) {
+				throw new IllegalArgumentException(
+						"the given old code fragment is not null, but that in the core is null");
+			} else if (!this.core.getOldCodeFragment().equals(
+					oldCodeFragment.getCore())) {
+				throw new IllegalArgumentException(
+						"the given old code fragment does not match to that in the core");
+			}
+		} else if (this.core.getOldCodeFragment() != null) {
+			throw new IllegalArgumentException(
+					"the given old code fragment is null, but that in the core is not");
+		}
+
+		this.oldCodeFragment = oldCodeFragment;
+	}
+
+	/**
+	 * Get the new code fragment of this mapping.
+	 * 
+	 * @return the new code fragment of this mapping
+	 * 
+	 * @throws IllegalStateException
+	 *             if the old code fragment and/or the new code fragment have
+	 *             not been set
+	 */
+	public CodeFragment<E> getNewCodeFragment() {
+		if (oldCodeFragment == null && newCodeFragment == null) {
+			throw new IllegalStateException(
+					"the old code fragment and/or the new code fragment have not been set");
+		}
+
+		return this.newCodeFragment;
+	}
+	
+	/**
+	 * Set the new code fragment of this mapping with the specified one.
+	 * 
+	 * @param newCodeFragment
+	 *            the new code fragment to be set
+	 * 
+	 * @throws IllegalArgumentException
+	 *             if any of the following statements hold: (1) the given 
+	 *             code fragment is <code>null</code> but that in the core is
+	 *             not <code>null</code>, (2) the given  code fragment is not
+	 *             <code>null</code> but that in the core is <code>null</code>,
+	 *             (3) the given  code fragment does not match to that in the
+	 *             core
+	 */
+	public void setNewCodeFragment(final CodeFragment<E> newCodeFragment) {
+		if (newCodeFragment != null) {
+			if (this.core.getNewCodeFragment() == null) {
+				throw new IllegalArgumentException(
+						"the given new code fragment is not null, but that in the core is null");
+			} else if (!this.core.getNewCodeFragment().equals(
+					newCodeFragment.getCore())) {
+				throw new IllegalArgumentException(
+						"the given new code fragment does not match to that in the core");
+			}
+		} else if (this.core.getNewCodeFragment() != null) {
+			throw new IllegalArgumentException(
+					"the given new code fragment is null, but that in the core is not");
+		}
+
+		this.newCodeFragment = newCodeFragment;
+	}
+
+}
