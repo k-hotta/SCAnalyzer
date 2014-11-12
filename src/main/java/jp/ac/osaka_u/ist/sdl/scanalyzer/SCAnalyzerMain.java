45020d0cbcdbcc80384dbe96516b15533d85ffbb
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-12 11:37:59 +0100

    rename the deafult configuration file

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/SCAnalyzerMain.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/SCAnalyzerMain.java
@@ -12,12 +12,10 @@ public class SCAnalyzerMain {
 	 * This is the path to the default configuration file. If no other file has
 	 * been specified, the default file will be loaded.
 	 */
-	public static final String DEFAULT_CONFIG_FILE = "scanalyzer-settings.xml";
+	public static final String DEFAULT_CONFIG_FILE = "scanalyzer-config.xml";
 
 	/**
-	 * This is the main method of SCAnalyzer. This method creates a new instance
-	 * of {@link SCAnalyzerMain} and then delegate all the necessary processing
-	 * to the instance.
+	 * This is the main method of SCAnalyzer.
 	 * 
 	 * @param args
 	 */
@@ -26,11 +24,12 @@ public class SCAnalyzerMain {
 		 * create a new instance of this class and then delegate all the
 		 * necessary processing to the instance
 		 */
-		(new SCAnalyzerMain()).run(args);
+		final SCAnalyzerMain main = new SCAnalyzerMain();
+		main.run(args);
 	}
 
 	public void run(final String[] args) {
-
+		loadConfig(args);
 	}
 
 	private void loadConfig(final String[] args) {
