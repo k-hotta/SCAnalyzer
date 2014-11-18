063a8c03bf9a17326cb59d6fb66cb5075400bec0
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-18 16:51:31 +0100

    add an interface to represent constants of UI

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/ui/UIConstants.java
@@ -0,0 +1,17 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.ui;
+
+import java.awt.Color;
+
+/**
+ * This interface contains setting values of UI.
+ * 
+ * @author k-hotta
+ *
+ */
+public interface UIConstants {
+
+	public static final Color CLONED_SEGMENT_HIGHLIGHT_COLOR = new Color(255, 100, 100);
+
+	public static final int LINE_MERGIN = 3;
+	
+}
