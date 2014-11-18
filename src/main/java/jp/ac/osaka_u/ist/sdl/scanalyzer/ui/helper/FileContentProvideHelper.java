be9e9dc18c8e5bda9133c4c018d04b86e127fcff
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-18 16:10:14 +0100

    add a helper class to get file contents

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/ui/helper/FileContentProvideHelper.java
@@ -0,0 +1,57 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.ui.helper;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Segment;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.SourceFile;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.io.in.IFileContentProvider;
+
+/**
+ * This class supports to get file contents.
+ * 
+ * @author k-hotta
+ *
+ */
+public class FileContentProvideHelper {
+
+	/**
+	 * The file content provider
+	 */
+	private static IFileContentProvider<?> provider = null;
+
+	/**
+	 * Set the file content provider. This method must be called before calling
+	 * any methods in this class.
+	 * 
+	 * @param provider
+	 *            the provider to be set
+	 */
+	public static void setProvider(final IFileContentProvider<?> provider) {
+		if (provider == null) {
+			throw new IllegalArgumentException("the given provider is null");
+		}
+
+		FileContentProvideHelper.provider = provider;
+	}
+
+	/**
+	 * Get the content of the owner file of the given segment as a string.
+	 * 
+	 * @param segment
+	 *            the segment under consideration
+	 * 
+	 * @return a string represents the content of the whole of the owner file
+	 */
+	public static String getFileContent(final Segment<?> segment) {
+		try {
+			final SourceFile<?> sourceFile = segment.getSourceFile();
+			final Version<?> version = segment.getCodeFragment()
+					.getCloneClass().getVersion();
+
+			return provider.getFileContent(version.getCore(),
+					sourceFile.getCore());
+		} catch (Exception e) {
+			throw new IllegalStateException("cannot get the file content", e);
+		}
+	}
+
+}
