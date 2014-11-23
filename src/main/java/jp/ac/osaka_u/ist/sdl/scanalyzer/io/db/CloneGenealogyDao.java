7bf62b0f398be58e19bf46017322d6a0d5965586
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-23 16:31:33 +0100

    implement refreshChlidren/refreshChildrenForAll in CloneGenealogyDao

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/db/CloneGenealogyDao.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/db/CloneGenealogyDao.java
@@ -3,13 +3,18 @@ package jp.ac.osaka_u.ist.sdl.scanalyzer.io.db;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.concurrent.Callable;
 
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IDGenerator;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCloneClassMapping;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCloneGenealogy;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCloneGenealogyCloneClassMapping;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBVersion;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -102,16 +107,70 @@ public class CloneGenealogyDao extends AbstractDataDao<DBCloneGenealogy> {
 	protected DBCloneGenealogy refreshChildren(DBCloneGenealogy element)
 			throws Exception {
 		if (deepRefresh) {
-			versionDao.refresh(element.getStartVersion());
-			versionDao.refresh(element.getEndVersion());
+			final Set<DBVersion> versionsToBeRefreshed = new HashSet<>();
+			versionsToBeRefreshed.add(element.getStartVersion());
+			versionsToBeRefreshed.add(element.getEndVersion());
+			versionDao.refreshAll(versionsToBeRefreshed);
+			element.setStartVersion(versionDao.get(element.getStartVersion()
+					.getId()));
+			element.setEndVersion(versionDao.get(element.getEndVersion()
+					.getId()));
 		}
 
-		element.setCloneClassMappings(getCorrespondingCloneClassMappings(element));
+		final Set<DBCloneClassMapping> cloneClassMappingsToBeRetrieved = new HashSet<>();
+		final Collection<DBCloneClassMapping> mappingsInElement = getCorrespondingCloneClassMappings(element);
+		cloneClassMappingsToBeRetrieved.addAll(mappingsInElement);
+
+		cloneClassMappingDao.refreshAll(cloneClassMappingsToBeRetrieved);
+		final Collection<DBCloneClassMapping> toBeStored = new ArrayList<>();
+		for (final DBCloneClassMapping mappingInElement : mappingsInElement) {
+			toBeStored.add(cloneClassMappingDao.get(mappingInElement.getId()));
+		}
 
 		return element;
 	}
 
-	public Collection<DBCloneClassMapping> getCorrespondingCloneClassMappings(
+	@Override
+	protected Collection<DBCloneGenealogy> refreshChildrenForAll(
+			Collection<DBCloneGenealogy> elements) throws Exception {
+		if (deepRefresh) {
+			final Set<DBVersion> versionsToBeRefreshed = new HashSet<>();
+			for (final DBCloneGenealogy element : elements) {
+				versionsToBeRefreshed.add(element.getStartVersion());
+				versionsToBeRefreshed.add(element.getEndVersion());
+			}
+			versionDao.refreshAll(versionsToBeRefreshed);
+			for (final DBCloneGenealogy element : elements) {
+				element.setStartVersion(versionDao.get(element
+						.getStartVersion().getId()));
+				element.setEndVersion(versionDao.get(element.getEndVersion()
+						.getId()));
+			}
+		}
+
+		final Set<DBCloneClassMapping> cloneClassMappingsToBeRetrieved = new HashSet<>();
+		final Map<Long, Collection<DBCloneClassMapping>> cloneClassMappingInElements = new TreeMap<>();
+		for (final DBCloneGenealogy element : elements) {
+			final Collection<DBCloneClassMapping> mappingsInElement = getCorrespondingCloneClassMappings(element);
+			cloneClassMappingsToBeRetrieved.addAll(mappingsInElement);
+			cloneClassMappingInElements.put(element.getId(), mappingsInElement);
+		}
+		cloneClassMappingDao.refreshAll(cloneClassMappingsToBeRetrieved);
+		for (final DBCloneGenealogy element : elements) {
+			final Collection<DBCloneClassMapping> mappingsInElement = cloneClassMappingInElements
+					.get(element.getId());
+			final Collection<DBCloneClassMapping> toBeStored = new ArrayList<>();
+			for (final DBCloneClassMapping mappingInElement : mappingsInElement) {
+				toBeStored.add(cloneClassMappingDao.get(mappingInElement
+						.getId()));
+			}
+			element.setCloneClassMappings(toBeStored);
+		}
+
+		return elements;
+	}
+
+	private Collection<DBCloneClassMapping> getCorrespondingCloneClassMappings(
 			final DBCloneGenealogy cloneGenealogy) throws Exception {
 		if (cloneClassMappingsForCloneGenealogyQuery == null) {
 			cloneClassMappingsForCloneGenealogyQuery = makeCloneClassMappingsForCloneGenealogyQuery();
@@ -119,8 +178,8 @@ public class CloneGenealogyDao extends AbstractDataDao<DBCloneGenealogy> {
 		cloneClassMappingsForCloneGenealogyQuery.setArgumentHolderValue(0,
 				cloneGenealogy);
 
-		return cloneClassMappingDao
-				.query(cloneClassMappingsForCloneGenealogyQuery);
+		return cloneClassMappingDao.query(
+				cloneClassMappingsForCloneGenealogyQuery, false);
 	}
 
 	private PreparedQuery<DBCloneClassMapping> makeCloneClassMappingsForCloneGenealogyQuery()
