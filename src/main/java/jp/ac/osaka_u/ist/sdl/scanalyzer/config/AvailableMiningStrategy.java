ab711552185fc42b6baf50deb6887330866c074c
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 21:21:33 +0100

    implement canBe()

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/AvailableMiningStrategy.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/AvailableMiningStrategy.java
@@ -4,4 +4,25 @@ public enum AvailableMiningStrategy {
 
 	GENEALOGY_PERSIST_PERIOD;
 
+	/**
+	 * Provide a string that represents which values can be used as
+	 * AvailableMiningStrategy.
+	 * 
+	 * @return a string representing available values
+	 */
+	public static String canBe() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("{ ");
+
+		for (final AvailableMiningStrategy value : values()) {
+			builder.append(value.toString() + ", ");
+		}
+		builder.deleteCharAt(builder.length() - 1);
+		builder.deleteCharAt(builder.length() - 1);
+
+		builder.append(" }");
+
+		return builder.toString();
+	}
+
 }
