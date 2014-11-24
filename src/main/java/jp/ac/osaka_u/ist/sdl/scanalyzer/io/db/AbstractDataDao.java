31fa1dce02d7618cb27d138163a2224b37ff1498
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 10:33:45 +0100

    [MINOR_CHANGE] remove unused import statements

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/db/AbstractDataDao.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/db/AbstractDataDao.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
-import java.util.SortedSet;
-import java.util.TreeSet;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
