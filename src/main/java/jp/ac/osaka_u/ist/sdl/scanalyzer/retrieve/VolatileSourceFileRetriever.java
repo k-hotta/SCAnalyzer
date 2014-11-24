a31af01602876a67dc2b24d818e7fce2c15f9da3
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 19:09:44 +0100

    implement retriever for source file

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileSourceFileRetriever.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileSourceFileRetriever.java
@@ -3,14 +3,74 @@ package jp.ac.osaka_u.ist.sdl.scanalyzer.retrieve;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.SourceFile;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBSourceFile;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBVersion;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.io.in.IFileContentProvider;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.io.in.ISourceFileParser;
 
+/**
+ * This is a retriever for {@link SourceFile} with volatile information. Note
+ * that it is impossible to retrieve file contents only from an instance of
+ * {@link DBSourceFile} because it has no information about in which version the
+ * file exists. Hence {@link this#retrieveElement(DBSourceFile)} does not work.
+ * This class therefore provides another method {@link
+ * this#retrieveElement(DBSourceFile, DBVersion)} to completely retrieve file
+ * contents.
+ * 
+ * @author k-hotta
+ *
+ * @param <E>
+ *            the type of program element
+ */
 public class VolatileSourceFileRetriever<E extends IProgramElement> implements
 		IRetriever<E, DBSourceFile, SourceFile<E>> {
 
-	@Override
-	public SourceFile<E> retrieveElement(DBSourceFile dbElement) {
-		// TODO Auto-generated method stub
-		return null;
+	/**
+	 * The manager for retrieved objects
+	 */
+	private final RetrievedObjectManager<E> manager;
+
+	/**
+	 * The file content provider
+	 */
+	private final IFileContentProvider<E> fileContentProvider;
+
+	/**
+	 * 
+	 */
+	private final ISourceFileParser<E> parser;
+
+	public VolatileSourceFileRetriever(final RetrievedObjectManager<E> manager,
+			final IFileContentProvider<E> fileContentProvider,
+			final ISourceFileParser<E> parser) {
+		this.manager = manager;
+		this.fileContentProvider = fileContentProvider;
+		this.parser = parser;
 	}
 
+	/**
+	 * @deprecated This method produces an instance of {@link SourceFile} with
+	 *             no contents. Use
+	 *             {@link VolatileSourceFileRetriever#retrieveElement(DBSourceFile, DBVersion)}
+	 *             instead.
+	 */
+	@Override
+	public SourceFile<E> retrieveElement(DBSourceFile dbElement) {
+		return retrieveElement(dbElement, null);
+	}
+
+	public SourceFile<E> retrieveElement(DBSourceFile dbElement,
+			DBVersion dbVersion) {
+		final SourceFile<E> sourceFile = new SourceFile<E>(dbElement);
+
+		if (dbVersion != null) {
+			final String fileContentsStr = fileContentProvider.getFileContent(
+					dbVersion, dbElement);
+			sourceFile.setContents(parser.parse(sourceFile, fileContentsStr)
+					.values());
+		}
+
+		manager.add(sourceFile);
+
+		return sourceFile;
+	}
 }
