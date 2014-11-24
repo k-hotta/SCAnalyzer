9d6a00ba05f059baaa716ce0a81678354681925e
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 19:09:19 +0100

    implement retriever for revision

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileRevisionRetriever.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileRevisionRetriever.java
@@ -4,13 +4,31 @@ import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Revision;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBRevision;
 
+/**
+ * This is a retriever for {@link Revision}.
+ * 
+ * @author k-hotta
+ *
+ * @param <E>
+ *            the type of program element
+ */
 public class VolatileRevisionRetriever<E extends IProgramElement> implements
 		IRetriever<E, DBRevision, Revision> {
 
+	/**
+	 * The manager for retrieved objects
+	 */
+	private final RetrievedObjectManager<E> manager;
+
+	public VolatileRevisionRetriever(final RetrievedObjectManager<E> manager) {
+		this.manager = manager;
+	}
+
 	@Override
 	public Revision retrieveElement(DBRevision dbElement) {
-		// TODO Auto-generated method stub
-		return null;
+		final Revision revision = new Revision(dbElement);
+		manager.add(revision);
+		return revision;
 	}
 
 }
