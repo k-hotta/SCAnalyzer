cde7c56f148583c15f3d312af6b6559b7ba45dc3
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 16:58:31 +0100

    implement retriever for clone class

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileCloneClassRetriever.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileCloneClassRetriever.java
@@ -1,16 +1,96 @@
 package jp.ac.osaka_u.ist.sdl.scanalyzer.retrieve;
 
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CloneClass;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CodeFragment;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCloneClass;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBCodeFragment;
 
+/**
+ * This is a retriever for {@link CloneClass} with the volatile information.
+ * 
+ * @author k-hotta
+ *
+ * @param <E>
+ *            the type of program elements
+ */
 public class VolatileCloneClassRetriever<E extends IProgramElement> implements
 		IRetriever<E, DBCloneClass, CloneClass<E>> {
 
+	/**
+	 * The manager for retrieved objects
+	 */
+	private final RetrievedObjectManager<E> manager;
+
+	/**
+	 * The retriever for code fragments
+	 */
+	private VolatileCodeFragmentRetriever<E> codeFragmentRetriever;
+
+	/**
+	 * The retriever for versions
+	 */
+	private VolatileVersionRetriever<E> versionRetriever;
+
+	public VolatileCloneClassRetriever(final RetrievedObjectManager<E> manager) {
+		this.manager = manager;
+		this.codeFragmentRetriever = null;
+		this.versionRetriever = null;
+	}
+
+	/**
+	 * Set the code fragment retriever
+	 * 
+	 * @param codeFragmentRetriever
+	 *            the retriever to be set
+	 */
+	public void setCodeFragmentRetriever(
+			final VolatileCodeFragmentRetriever<E> codeFragmentRetriever) {
+		this.codeFragmentRetriever = codeFragmentRetriever;
+	}
+
+	/**
+	 * Set the version retriever
+	 * 
+	 * @param versionRetriever
+	 *            the retriever to be set
+	 */
+	public void setVersionRetriever(
+			final VolatileVersionRetriever<E> versionRetriever) {
+		this.versionRetriever = versionRetriever;
+	}
+
 	@Override
 	public CloneClass<E> retrieveElement(DBCloneClass dbElement) {
-		// TODO Auto-generated method stub
-		return null;
+		final CloneClass<E> cloneClass = new CloneClass<E>(dbElement);
+
+		// set cloned & ghost code fragments
+		for (final DBCodeFragment dbCodeFragment : dbElement.getCodeFragments()) {
+			CodeFragment<E> codeFragment = manager
+					.getCodeFragment(dbCodeFragment.getId());
+
+			if (codeFragment == null) {
+				codeFragment = codeFragmentRetriever
+						.retrieveElement(dbCodeFragment);
+			}
+
+			cloneClass.addCodeFragment(codeFragment);
+			codeFragment.setCloneClass(cloneClass);
+		}
+
+		// set version
+		Version<E> version = manager.getVersion(dbElement.getVersion().getId());
+
+		if (version == null) {
+			version = versionRetriever.retrieveElement(dbElement.getVersion());
+		}
+
+		cloneClass.setVersion(version);
+
+		manager.add(cloneClass);
+
+		return cloneClass;
 	}
 
 }
