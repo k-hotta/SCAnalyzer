54310b4d509b114c21b9c80f481ac42ac4cb9ef4
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-24 19:09:55 +0100

    implement retriever for version

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileVersionRetriever.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/retrieve/VolatileVersionRetriever.java
@@ -1,16 +1,84 @@
 package jp.ac.osaka_u.ist.sdl.scanalyzer.retrieve;
 
+import org.tmatesoft.sqljet.core.internal.lang.SqlParser.reindex_stmt_return;
+
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Revision;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBRevision;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.DBVersion;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.io.db.DBManager;
 
 public class VolatileVersionRetriever<E extends IProgramElement> implements
 		IRetriever<E, DBVersion, Version<E>> {
 
+	/**
+	 * The manager for retrieved objects
+	 */
+	private final RetrievedObjectManager<E> manager;
+
+	/**
+	 * The manager for database connection
+	 */
+	private final DBManager dbManager;
+
+	/**
+	 * The retriever for revisions
+	 */
+	private VolatileRevisionRetriever<E> revisionRetriever;
+
+	public VolatileVersionRetriever(final RetrievedObjectManager<E> manager) {
+		this.manager = manager;
+		this.dbManager = DBManager.getInstance();
+		this.revisionRetriever = null;
+	}
+
+	/**
+	 * Set the revision retriever
+	 * 
+	 * @param revisionRetriever
+	 *            the retriever to be set
+	 */
+	public void setRevisionRetriever(
+			final VolatileRevisionRetriever<E> revisionRetriever) {
+		this.revisionRetriever = revisionRetriever;
+	}
+
 	@Override
 	public Version<E> retrieveElement(DBVersion dbElement) {
-		// TODO Auto-generated method stub
-		return null;
+		final Version<E> version = new Version<E>(dbElement);
+
+		// refresh the db version
+		// this is required because it might not be refreshed
+		// when deep refreshing is OFF
+		try {
+			dbManager.getNativeDao(DBVersion.class).refresh(dbElement);
+		} catch (Exception e) {
+			throw new IllegalStateException("cannot refresh the version "
+					+ dbElement.getId(), e);
+		}
+
+		Revision revision = manager
+				.getRevision(dbElement.getRevision().getId());
+
+		if (revision == null) {
+			try {
+				dbManager.getNativeDao(DBRevision.class).refresh(
+						dbElement.getRevision());
+			} catch (Exception e) {
+				throw new IllegalStateException("cannot refresh the revision "
+						+ dbElement.getRevision().getIdentifier(), e);
+			}
+
+			revision = revisionRetriever.retrieveElement(dbElement
+					.getRevision());
+		}
+
+		version.setRevision(revision);
+
+		manager.add(version);
+
+		return version;
 	}
 
 }
