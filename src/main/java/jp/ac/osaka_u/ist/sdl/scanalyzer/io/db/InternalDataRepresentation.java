3923880c1260671e3651c73c35f47b10fc8e632e
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-11-28 11:30:18 +0100

    add an interface to represent a row of database tables

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/io/db/InternalDataRepresentation.java
@@ -0,0 +1,26 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.io.db;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.IDBElement;
+
+/**
+ * This interface describes an internal representation of the data type
+ * specified as its type argument. An instance of classes implementing this
+ * interface is expected to represent a row of a database table. This interface
+ * provides only a single method {@link #getId()} because the ID field is the
+ * only column shared among all the database tables.
+ * 
+ * @author k-hotta
+ *
+ * @param <D>
+ *            the type of data to be represented
+ */
+public interface InternalDataRepresentation<D extends IDBElement> {
+
+	/**
+	 * Get the id of the element.
+	 * 
+	 * @return the id of the element
+	 */
+	public Long getId();
+
+}
