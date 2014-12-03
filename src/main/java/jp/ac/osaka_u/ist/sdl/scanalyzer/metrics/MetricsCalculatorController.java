4c554280489c74029830673795520e70e7997e96
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-03 14:12:32 +0100

    add a contorller class for metrics calculator

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/MetricsCalculatorController.java
@@ -0,0 +1,37 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.metrics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CloneClassMapping;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
+
+/**
+ * This class controls to run multiple metrics calculators.
+ * 
+ * @author k-hotta
+ *
+ * @param <E>
+ */
+public class MetricsCalculatorController<E extends IProgramElement> {
+
+	private final List<IMetricsCalculator<E>> calculators;
+
+	public MetricsCalculatorController() {
+		this.calculators = new ArrayList<>();
+	}
+
+	public void addCalculator(final IMetricsCalculator<E> calculator) {
+		this.calculators.add(calculator);
+	}
+
+	public void calculateAll(final Version<E> previous, final Version<E> next,
+			final Collection<CloneClassMapping<E>> mappings) {
+		for (final IMetricsCalculator<E> calculator : calculators) {
+			calculator.calculate(previous, next, mappings);
+		}
+	}
+
+}
