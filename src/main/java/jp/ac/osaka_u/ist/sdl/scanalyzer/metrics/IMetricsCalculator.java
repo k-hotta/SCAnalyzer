e0fed37002871b9b772e2703796a600e8d2545b4
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-03 13:56:32 +0100

    make sure IMetricsCalculator is a functional interface

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/IMetricsCalculator.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/IMetricsCalculator.java
@@ -12,11 +12,14 @@ import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
  * @author k-hotta
  *
  */
+@FunctionalInterface
 public interface IMetricsCalculator<E extends IProgramElement> {
 
 	/**
-	 * Calculate metrics. All the calculated values must be stored in
-	 * DBElements, otherwise the values will not be persist.
+	 * Calculate metrics. If the calculated values are stored as fields of
+	 * DBElements, the values will be stored into database. If it is not the
+	 * case, the implementations of this method should output the results in
+	 * their own ways, otherwise the results will not be reported at all.
 	 * 
 	 * @param previous
 	 *            the previous version
