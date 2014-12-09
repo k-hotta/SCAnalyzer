2e5ac7b16a4874a81434a9f0ed354e74d9849e76
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-09 19:40:42 +0100

    include the tasks to detect modifications

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/CloneClassSimilarityAnalyzer.java
@@ -0,0 +1,225 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.metrics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.SortedSet;
+import java.util.TreeMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CloneClass;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CodeFragment;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.util.LCSFinder;
+import difflib.myers.Equalizer;
+
+/**
+ * This class represents a task to calculate metrics for a given clone class.
+ * 
+ * @author k-hotta
+ *
+ */
+public class CloneClassSimilarityAnalyzer<E extends IProgramElement> {
+
+	/**
+	 * The clone classes
+	 */
+	private final Collection<CloneClass<E>> cloneClasses;
+
+	/**
+	 * This map contains all the elements in code fragments
+	 */
+	private final ConcurrentMap<Long, Map<Long, List<E>>> allElements;
+
+	/**
+	 * This map contains the information about which elements are included in
+	 * LCS between CLONED fragments for each clone class. The keys are the ids
+	 * of clone classes, and the values are maps between fragment ids and
+	 * elements in each of the fragment.
+	 */
+	private final ConcurrentMap<Long, Map<Long, List<E>>> lcsElementsInCloned;
+
+	/**
+	 * This map contains the information about which elements are included in
+	 * LCS between ALL fragments for each clone class. The keys are the ids of
+	 * clone classes, and the values are maps between fragment ids and elements
+	 * in each of the fragment.
+	 */
+	private final ConcurrentMap<Long, Map<Long, List<E>>> lcsElementsInAll;
+
+	/**
+	 * The equalizer to detect LCS
+	 */
+	private final Equalizer<E> equalizer;
+
+	public CloneClassSimilarityAnalyzer(
+			final Collection<CloneClass<E>> cloneClasses,
+			final ConcurrentMap<Long, Map<Long, List<E>>> allElements,
+			final ConcurrentMap<Long, Map<Long, List<E>>> lcsElementsInCloned,
+			final ConcurrentMap<Long, Map<Long, List<E>>> lcsElementsInAll,
+			final Equalizer<E> equalizer) {
+		this.cloneClasses = cloneClasses;
+		this.allElements = allElements;
+		this.lcsElementsInCloned = lcsElementsInCloned;
+		this.lcsElementsInAll = lcsElementsInAll;
+		this.equalizer = equalizer;
+	}
+
+	public void analyze() {
+		final ExecutorService pool = Executors.newCachedThreadPool();
+
+		try {
+			final List<Future<?>> futures = new ArrayList<>();
+			for (final CloneClass<E> cloneClass : cloneClasses) {
+				final SimilarityAnalyzeTask task = new SimilarityAnalyzeTask(
+						cloneClass);
+				futures.add(pool.submit(task));
+			}
+
+			for (final Future<?> future : futures) {
+				try {
+					future.get();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		} finally {
+			pool.shutdown();
+		}
+	}
+
+	private class SimilarityAnalyzeTask implements Runnable {
+
+		/**
+		 * the clone class under consideration
+		 */
+		private final CloneClass<E> cloneClass;
+
+		private SimilarityAnalyzeTask(final CloneClass<E> cloneClass) {
+			this.cloneClass = cloneClass;
+		}
+
+		public void run() {
+			final Map<Long, CodeFragment<E>> clonedFragments = cloneClass
+					.getCodeFragments();
+			final Map<Long, CodeFragment<E>> ghostFragments = cloneClass
+					.getGhostFragments();
+
+			// elements included in LCS
+			final Map<Long, List<E>> clonedFragmentElements = LCSFinder
+					.getFragmentElements(clonedFragments);
+			final Map<Long, List<E>> ghostFragmentElements = LCSFinder
+					.getFragmentElements(ghostFragments);
+
+			final Map<Long, List<E>> allFragmentElements = new TreeMap<>();
+			allFragmentElements.putAll(clonedFragmentElements);
+			allFragmentElements.putAll(ghostFragmentElements);
+
+			/*
+			 * find LCS among all the CLONED fragments
+			 */
+			final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs = findLcsInCloned(clonedFragmentElements);
+
+			final Long firstKey = clonedFragmentElementsInCloneLcs.firstKey();
+			final int numCommonClonedElements = (firstKey == null) ? 0
+					: clonedFragmentElementsInCloneLcs.get(firstKey).size();
+
+			/*
+			 * find LCS among the LCS of cloned fragments and all the ghost
+			 * fragments
+			 */
+			final SortedMap<Long, List<E>> fragmentElementsInAllLcs = findLcsInAll(
+					ghostFragmentElements, clonedFragmentElementsInCloneLcs,
+					firstKey);
+
+			final int numCommonAllElements = fragmentElementsInAllLcs.get(
+					fragmentElementsInAllLcs.firstKey()).size();
+
+			// record the metric values for clone classes
+			cloneClass.getCore().setNumCommonClonedElements(
+					numCommonClonedElements);
+			cloneClass.getCore().setNumCommonAllElements(numCommonAllElements);
+
+			// store the information of LCSs
+			allElements.put(cloneClass.getId(), allFragmentElements);
+			lcsElementsInCloned.put(cloneClass.getId(),
+					clonedFragmentElementsInCloneLcs);
+			lcsElementsInAll.put(cloneClass.getId(), fragmentElementsInAllLcs);
+		}
+
+		private SortedMap<Long, List<E>> findLcsInCloned(
+				final Map<Long, List<E>> clonedFragmentElements) {
+			final Map<Long, SortedSet<Integer>> lcsInCloned = LCSFinder
+					.detectLcs(clonedFragmentElements, equalizer);
+
+			final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs = new TreeMap<>();
+
+			for (final Map.Entry<Long, SortedSet<Integer>> entry : lcsInCloned
+					.entrySet()) {
+				final List<E> all = clonedFragmentElements.get(entry.getKey());
+				final List<E> newList = new ArrayList<>();
+
+				for (final Integer index : entry.getValue()) {
+					newList.add(all.get(index));
+				}
+
+				clonedFragmentElementsInCloneLcs.put(entry.getKey(), newList);
+			}
+
+			return clonedFragmentElementsInCloneLcs;
+		}
+
+		private SortedMap<Long, List<E>> findLcsInAll(
+				final Map<Long, List<E>> ghostFragmentElements,
+				final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs,
+				final Long firstKey) {
+			// add the LCS of cloned fragments into target
+			if (firstKey != null) {
+				ghostFragmentElements.put(firstKey,
+						clonedFragmentElementsInCloneLcs.get(firstKey));
+			}
+
+			final Map<Long, SortedSet<Integer>> lcsInAll = LCSFinder.detectLcs(
+					ghostFragmentElements, equalizer);
+			final SortedMap<Long, List<E>> fragmentElementsInAllLcs = new TreeMap<>();
+
+			for (final Map.Entry<Long, SortedSet<Integer>> entry : lcsInAll
+					.entrySet()) {
+				final List<E> all = ghostFragmentElements.get(entry.getKey());
+				final List<E> newList = new ArrayList<>();
+
+				for (final Integer index : entry.getValue()) {
+					newList.add(all.get(index));
+				}
+
+				if (entry.getKey() == firstKey) {
+					// special treat for the LCS among cloned fragments
+					for (final Map.Entry<Long, List<E>> clonedEntry : clonedFragmentElementsInCloneLcs
+							.entrySet()) {
+						if (clonedEntry.getKey() != firstKey) {
+							final List<E> allInCloned = clonedEntry.getValue();
+							final List<E> newListForCloned = new ArrayList<>();
+
+							for (final Integer index : entry.getValue()) {
+								newListForCloned.add(allInCloned.get(index));
+							}
+
+							fragmentElementsInAllLcs.put(clonedEntry.getKey(),
+									newListForCloned);
+						}
+					}
+				}
+
+				fragmentElementsInAllLcs.put(entry.getKey(), newList);
+			}
+			return fragmentElementsInAllLcs;
+		}
+
+	}
+
+}
--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/DefaultCloneClassMetricsCalculator.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/metrics/DefaultCloneClassMetricsCalculator.java
@@ -1,22 +1,12 @@
 package jp.ac.osaka_u.ist.sdl.scanalyzer.metrics;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.SortedMap;
-import java.util.SortedSet;
-import java.util.TreeMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ConcurrentSkipListMap;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
-import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CloneClass;
-import jp.ac.osaka_u.ist.sdl.scanalyzer.data.CodeFragment;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IProgramElement;
 import jp.ac.osaka_u.ist.sdl.scanalyzer.data.Version;
-import jp.ac.osaka_u.ist.sdl.scanalyzer.util.LCSFinder;
 import difflib.myers.Equalizer;
 
 /**
@@ -32,9 +22,14 @@ public class DefaultCloneClassMetricsCalculator<E extends IProgramElement>
 		implements IMetricsCalculator<E> {
 
 	/**
-	 * The equalizer of elements
+	 * The equalizer of elements to detect LCS
 	 */
-	private final Equalizer<E> equalizer;
+	private final Equalizer<E> equalizerForLcs;
+
+	/**
+	 * The equalizer of elements to detect diff
+	 */
+	private final Equalizer<E> equalizerForDiff;
 
 	/**
 	 * This map contains all the elements in code fragments
@@ -57,8 +52,11 @@ public class DefaultCloneClassMetricsCalculator<E extends IProgramElement>
 	 */
 	private final ConcurrentMap<Long, Map<Long, List<E>>> lcsElementsInAll;
 
-	public DefaultCloneClassMetricsCalculator(final Equalizer<E> equalizer) {
-		this.equalizer = equalizer;
+	public DefaultCloneClassMetricsCalculator(
+			final Equalizer<E> equalizerForLcs,
+			final Equalizer<E> equalizerForDiff) {
+		this.equalizerForLcs = equalizerForLcs;
+		this.equalizerForDiff = equalizerForDiff;
 		this.allElements = new ConcurrentSkipListMap<>();
 		this.lcsElementsInCloned = new ConcurrentSkipListMap<>();
 		this.lcsElementsInAll = new ConcurrentSkipListMap<>();
@@ -69,162 +67,23 @@ public class DefaultCloneClassMetricsCalculator<E extends IProgramElement>
 		this.lcsElementsInCloned.clear();
 		this.lcsElementsInAll.clear();
 
-		final Map<Long, CloneClass<E>> cloneClasses = next.getCloneClasses();
+		analyzeSimilarity(next);
 
-		final ExecutorService pool = Executors.newCachedThreadPool();
-
-		try {
-			final List<Future<?>> futures = new ArrayList<>();
-
-			for (final CloneClass<E> cloneClass : cloneClasses.values()) {
-				final CalculateTask task = new CalculateTask(cloneClass);
-				futures.add(pool.submit(task));
-			}
-
-			for (final Future<?> future : futures) {
-				try {
-					future.get();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		} finally {
-			pool.shutdown();
-		}
+		analyzeModifications(next);
 	}
 
-	/**
-	 * This class represents a task to calculate metrics for a given clone
-	 * class.
-	 * 
-	 * @author k-hotta
-	 *
-	 */
-	private class CalculateTask implements Runnable {
+	private void analyzeSimilarity(Version<E> next) {
+		final CloneClassSimilarityAnalyzer<E> similarityAnalizer = new CloneClassSimilarityAnalyzer<E>(
+				next.getCloneClasses().values(), allElements,
+				lcsElementsInCloned, lcsElementsInAll, equalizerForLcs);
+		similarityAnalizer.analyze();
+	}
 
-		private final CloneClass<E> cloneClass;
-
-		private CalculateTask(final CloneClass<E> cloneClass) {
-			this.cloneClass = cloneClass;
-		}
-
-		@Override
-		public void run() {
-			final Map<Long, CodeFragment<E>> clonedFragments = cloneClass
-					.getCodeFragments();
-			final Map<Long, CodeFragment<E>> ghostFragments = cloneClass
-					.getGhostFragments();
-
-			// elements included in LCS
-			final Map<Long, List<E>> clonedFragmentElements = LCSFinder
-					.getFragmentElements(clonedFragments);
-			final Map<Long, List<E>> ghostFragmentElements = LCSFinder
-					.getFragmentElements(ghostFragments);
-
-			final Map<Long, List<E>> allFragmentElements = new TreeMap<>();
-			allFragmentElements.putAll(clonedFragmentElements);
-			allFragmentElements.putAll(ghostFragmentElements);
-
-			/*
-			 * find LCS among all the CLONED fragments
-			 */
-			final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs = findLcsInCloned(clonedFragmentElements);
-
-			final Long firstKey = clonedFragmentElementsInCloneLcs.firstKey();
-			final int numCommonClonedElements = (firstKey == null) ? 0
-					: clonedFragmentElementsInCloneLcs.get(firstKey).size();
-
-			/*
-			 * find LCS among the LCS of cloned fragments and all the ghost
-			 * fragments
-			 */
-			final SortedMap<Long, List<E>> fragmentElementsInAllLcs = findLcsInAll(
-					ghostFragmentElements, clonedFragmentElementsInCloneLcs,
-					firstKey);
-
-			final int numCommonAllElements = fragmentElementsInAllLcs.get(
-					fragmentElementsInAllLcs.firstKey()).size();
-
-			// record the metric values for clone classes
-			cloneClass.getCore().setNumCommonClonedElements(
-					numCommonClonedElements);
-			cloneClass.getCore().setNumCommonAllElements(numCommonAllElements);
-
-			// store the information of LCSs
-			allElements.put(cloneClass.getId(), allFragmentElements);
-			lcsElementsInCloned.put(cloneClass.getId(),
-					clonedFragmentElementsInCloneLcs);
-			lcsElementsInAll.put(cloneClass.getId(), fragmentElementsInAllLcs);
-		}
-
-		private SortedMap<Long, List<E>> findLcsInCloned(
-				final Map<Long, List<E>> clonedFragmentElements) {
-			final Map<Long, SortedSet<Integer>> lcsInCloned = LCSFinder
-					.detectLcs(clonedFragmentElements, equalizer);
-
-			final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs = new TreeMap<>();
-
-			for (final Map.Entry<Long, SortedSet<Integer>> entry : lcsInCloned
-					.entrySet()) {
-				final List<E> all = clonedFragmentElements.get(entry.getKey());
-				final List<E> newList = new ArrayList<>();
-
-				for (final Integer index : entry.getValue()) {
-					newList.add(all.get(index));
-				}
-
-				clonedFragmentElementsInCloneLcs.put(entry.getKey(), newList);
-			}
-
-			return clonedFragmentElementsInCloneLcs;
-		}
-
-		private SortedMap<Long, List<E>> findLcsInAll(
-				final Map<Long, List<E>> ghostFragmentElements,
-				final SortedMap<Long, List<E>> clonedFragmentElementsInCloneLcs,
-				final Long firstKey) {
-			// add the LCS of cloned fragments into target
-			if (firstKey != null) {
-				ghostFragmentElements.put(firstKey,
-						clonedFragmentElementsInCloneLcs.get(firstKey));
-			}
-
-			final Map<Long, SortedSet<Integer>> lcsInAll = LCSFinder.detectLcs(
-					ghostFragmentElements, equalizer);
-			final SortedMap<Long, List<E>> fragmentElementsInAllLcs = new TreeMap<>();
-
-			for (final Map.Entry<Long, SortedSet<Integer>> entry : lcsInAll
-					.entrySet()) {
-				final List<E> all = ghostFragmentElements.get(entry.getKey());
-				final List<E> newList = new ArrayList<>();
-
-				for (final Integer index : entry.getValue()) {
-					newList.add(all.get(index));
-				}
-
-				if (entry.getKey() == firstKey) {
-					// special treat for the LCS among cloned fragments
-					for (final Map.Entry<Long, List<E>> clonedEntry : clonedFragmentElementsInCloneLcs
-							.entrySet()) {
-						if (clonedEntry.getKey() != firstKey) {
-							final List<E> allInCloned = clonedEntry.getValue();
-							final List<E> newListForCloned = new ArrayList<>();
-
-							for (final Integer index : entry.getValue()) {
-								newListForCloned.add(allInCloned.get(index));
-							}
-
-							fragmentElementsInAllLcs.put(clonedEntry.getKey(),
-									newListForCloned);
-						}
-					}
-				}
-
-				fragmentElementsInAllLcs.put(entry.getKey(), newList);
-			}
-			return fragmentElementsInAllLcs;
-		}
-
+	private void analyzeModifications(Version<E> next) {
+		final CloneClassModificationAnalyzer<E> modificationAnalyzer = new CloneClassModificationAnalyzer<>(
+				allElements, lcsElementsInCloned, lcsElementsInAll, next
+						.getCloneClassMappings().values(), equalizerForDiff);
+		modificationAnalyzer.run();
 	}
 
 }
