e21111820a9893a53a47bf4702aacbec0d6ff5d0
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-11 16:24:28 +0100

    add a new class to have constnats related to configuration

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/ConfigConstant.java
@@ -0,0 +1,15 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.config;
+
+/**
+ * This interface constants related to configuration.
+ * 
+ * @author k-hotta
+ *
+ */
+public interface ConfigConstant {
+
+	public static final String FILE_PATTERN_STRATEGY_NAME = "$$strategy_name$$";
+
+	public static final String FILE_PATTERN_DATABASE_NAME = "$$database_name$$";
+
+}
