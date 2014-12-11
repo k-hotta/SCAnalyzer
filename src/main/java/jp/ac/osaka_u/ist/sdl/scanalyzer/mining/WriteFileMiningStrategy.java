81c106fd3415116c0409f7d51ecdcf5b7698ae1a
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-11 16:10:02 +0100

    add an interface to represent a mining strategy that write the results to files

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/mining/WriteFileMiningStrategy.java
@@ -0,0 +1,39 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.mining;
+
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.IDataElement;
+import jp.ac.osaka_u.ist.sdl.scanalyzer.data.db.IDBElement;
+
+/**
+ * This interface represents a mining strategy that write the results to a local
+ * file.
+ * 
+ * @author k-hotta
+ *
+ * @param <D>
+ * @param <T>
+ */
+public interface WriteFileMiningStrategy<D extends IDBElement, T extends IDataElement<D>>
+		extends MiningStrategy<D, T> {
+
+	/**
+	 * Get the name of strategy.
+	 * 
+	 * @return
+	 */
+	public String getStrategyName();
+
+	/**
+	 * Get the name of database.
+	 * 
+	 * @return
+	 */
+	public String getDatabaseName();
+
+	/**
+	 * Get date as a string.
+	 * 
+	 * @return
+	 */
+	public String getDateStr();
+
+}
