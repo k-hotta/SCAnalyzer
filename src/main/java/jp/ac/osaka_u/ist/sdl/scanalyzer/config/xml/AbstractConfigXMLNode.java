105e52f0fae8c99a9999f2a09c4b2b3bcfce4736
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-11 17:42:42 +0100

    add an abstract class to represent xml node

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/xml/AbstractConfigXMLNode.java
@@ -0,0 +1,61 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.config.xml;
+
+import org.w3c.dom.Node;
+
+/**
+ * An abstract class that refers to xml nodes in the configuration file.
+ * 
+ * @author k-hotta
+ *
+ */
+public abstract class AbstractConfigXMLNode {
+
+	/**
+	 * The core node
+	 */
+	protected final Node node;
+
+	/**
+	 * The node name
+	 */
+	protected final String nodeName;
+
+	/**
+	 * Whether this node is required, which means that it is mandatory or not
+	 */
+	protected final boolean required;
+
+	/**
+	 * The value of this node. NOTE: this can be <code>null</code>
+	 */
+	protected String value;
+
+	public AbstractConfigXMLNode(final Node node, final String nodeName,
+			final boolean required) {
+		this.node = node;
+		this.nodeName = nodeName;
+		this.required = required;
+		this.value = null;
+	}
+
+	public final Node getNode() {
+		return node;
+	}
+
+	public final String getNodeName() {
+		return nodeName;
+	}
+
+	public final boolean isRequired() {
+		return required;
+	}
+
+	public final String getValue() {
+		return value;
+	}
+
+	public final void setValue(String value) {
+		this.value = value;
+	}
+
+}
