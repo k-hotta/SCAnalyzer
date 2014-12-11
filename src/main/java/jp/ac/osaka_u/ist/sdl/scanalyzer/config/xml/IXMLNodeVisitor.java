f27c57bdbf5fdf0cd7c012f1364b25ee408b4aea
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-11 17:46:28 +0100

    add an interface to represent visitors of xml nodes

---
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/xml/IXMLNodeVisitor.java
@@ -0,0 +1,13 @@
+package jp.ac.osaka_u.ist.sdl.scanalyzer.config.xml;
+
+/**
+ * This interface a visitor of xml nodes for configuration.
+ * 
+ * @author k-hotta
+ *
+ */
+public interface IXMLNodeVisitor {
+
+	
+	
+}
