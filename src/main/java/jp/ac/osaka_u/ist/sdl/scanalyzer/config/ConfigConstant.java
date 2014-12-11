13944fc40d63cedde7ec6601e785f70684dd0933
k-hotta <k-hotta@ist.osaka-u.ac.jp>
2014-12-11 17:31:08 +0100

    add node names to constants

--- src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/ConfigConstant.java
+++ src/main/java/jp/ac/osaka_u/ist/sdl/scanalyzer/config/ConfigConstant.java
@@ -12,4 +12,60 @@ public interface ConfigConstant {
 
 	public static final String FILE_PATTERN_DATABASE_NAME = "$$database_name$$";
 
+	public static final String NODE_NAME_ROOT = "config";
+
+	public static final String NODE_NAME_GENERAL = "general";
+
+	public static final String NODE_NAME_DBMS = "dbms";
+
+	public static final String NODE_NAME_OVERWRITE = "overwrite";
+
+	public static final String NODE_NAME_TARGET = "target";
+
+	public static final String NODE_NAME_LANGUAGE = "language";
+
+	public static final String NODE_NAME_ELEMENT = "element";
+
+	public static final String NODE_NAME_REPOSITORY = "repository";
+
+	public static final String NODE_NAME_RELATIVE = "relative";
+
+	public static final String NODE_NAME_VERSION_CONTROL = "version-control";
+
+	public static final String NODE_NAME_DATABASE = "database";
+
+	public static final String NODE_NAME_START = "start";
+
+	public static final String NODE_NAME_END = "end";
+
+	public static final String NODE_NAME_CLONE_DETECTION = "clone-detection";
+
+	public static final String NODE_NAME_DETECTOR = "detector";
+
+	public static final String NODE_NAME_RESULT_DIRECTORY = "result-directory";
+
+	public static final String NODE_NAME_FILENAME_FORMAT = "filename-format";
+
+	public static final String NODE_NAME_MAPPING = "mapping";
+
+	public static final String NODE_NAME_RELOCATION = "relocation";
+
+	public static final String NODE_NAME_EQUALIZER = "equalizer";
+
+	public static final String NODE_NAME_CLONE_MAPPING = "clone-mapping";
+
+	public static final String NODE_NAME_ELEMENT_MAPPING = "element-mapping";
+
+	public static final String NODE_NAME_MINING = "mining";
+
+	public static final String NODE_NAME_COMMON_CONFIGURATION = "common-configuration";
+
+	public static final String NODE_NAME_MAX_RETRIEVED = "max-retrieved";
+
+	public static final String NODE_NAME_OUTPUT_FILE_PATTERN = "output-file-pattern";
+
+	public static final String NODE_NAME_STRATEGIES = "strategies";
+
+	public static final String NODE_NAME_STRATEGY = "strategy";
+
 }
